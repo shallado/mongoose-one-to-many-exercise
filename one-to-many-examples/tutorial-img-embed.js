eecdc2a0273a8e5b61702cb08f93f1772337b0dd
Sandro Hallado <sandrohallado@yahoo.com>
2020-06-15 15:28:54 -0700

    Create a one to many example using embedded documents approach

--- one-to-many-examples/tutorial-img-embed.js
+++ one-to-many-examples/tutorial-img-embed.js
@@ -5,4 +5,27 @@ const tutorialInfo = {
   author: 'George Lucas' 
 };
 
-tutorial.createTutorial(tutorialInfo);
+async function execute() {
+  const tutorialOne = await tutorial.createTutorial(tutorialInfo);
+  const id = tutorialOne._id;
+  const imageInfoOne = {
+    path: 'sites/uploads/images/mongodb.png',
+    url: '/images/mongodb.png',
+    caption: 'MongoDB Database',
+    createdAt: 1000
+  };
+
+  tutorial.uploadImage(id, imageInfoOne);
+
+  const imageInfoTwo = {
+    path: 'sites/uploads/images/mongodb.png',
+    url: '/images/mongodb.png',
+    caption: 'MongoDB Database',
+    createdAt: 2000
+  };
+
+  tutorial.uploadImage(id, imageInfoTwo);
+
+}
+
+execute();
